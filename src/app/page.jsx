3b0531f743a0cb4bb19503079871323ac1685804
Timothy Snyders <timothymichaelsnyders93@gmail.com>
2026-05-01 21:21:41 +0200

    Homepage link fixed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -21,7 +21,7 @@ const Homepage = () => {
             <p className="md:text-xl">Welcome to my digital canvas, where innovation and creativity converge. With a keen eye for aesthetics and a mastery of code, my portfolio showcases a diverse collection of projects that reflect my commitment to excellence.</p>
             {/* BUTTONS */}
             <div className="flex gap-4 w-full">
-              <Link href="/profile">
+              <Link href="/portfolio">
                 <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">View My Work</button>
               </Link>
               <Link href="/contact">
